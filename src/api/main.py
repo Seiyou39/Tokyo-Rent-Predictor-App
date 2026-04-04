4299fa969d89a36605834b7fb3f0ba054093f8fc
Seiyou39 <39jingyang.zhang@gmail.com>
2026-04-04 13:06:58 +0900

    feat: use FastApi to mount static files and add a new homepage

--- src/api/main.py
+++ src/api/main.py
@@ -1,8 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from pydantic import BaseModel
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from src.model.predict import predict_single
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="frontend/static"), name="static")
+templates = Jinja2Templates(directory="frontend/templates")
 
 class HouseInput(BaseModel):
     area: float
@@ -12,6 +16,11 @@ class HouseInput(BaseModel):
     total_floors: float
     location: str
     layout: str
+
+@app.get("/")
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
 
 
 @app.post("/predict")
